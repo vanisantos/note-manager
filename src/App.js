195e721ccb25ad2fa38efd481c352fbbb8de31d8
Vanessa dos Santos <vanessa.santos2704@gmail.com>
2021-06-02 18:31:36 -0300

    create note component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {NoteList} from "./Components/NoteList"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form>
+        <input type="text" placeholder="Título"/>
+        <textarea placeholder="Escreva sua nota"/>
+        <button>Criar nota</button>
+      </form>
+      <NoteList/>
+    </section>
   );
 }
 
---
+++ src/Components/NoteList.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+
+export class NoteList extends Component {
+  render(){
+    return(
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+    )
+  }
+}
